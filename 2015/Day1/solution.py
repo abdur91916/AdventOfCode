628a30fa33fab53de372a3b2def7fd131761a283
abdur <abdur91916@outlook.com>
2021-12-02 12:34:18 +0000

    day 1 2015 part 2 solution

--- 2015/Day1/solution.py
+++ 2015/Day1/solution.py
@@ -15,7 +15,23 @@ class Solution:
                 print("error cannot parse " + x)
         print("The answer to Day 1 part one is " + str(floor))
 
+    def partTwo(self):
+        numberOfChars = len(self.instructions)
+        floor = 0
+        for i in range(numberOfChars):
+            instruction = self.instructions[i]
+            if instruction == "(":
+                floor = floor + 1
+            elif instruction == ")":
+                floor = floor - 1
+            else:
+                print("error cannot parse " + instruction)
+
+            if floor == -1:
+                print("The answer to Day 1 part two is " + str(i + 1))
+                break
+
 
 sol = Solution()
 sol.partOne()
-
+sol.partTwo()
